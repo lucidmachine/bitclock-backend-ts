095e4d21af5c4f0b123af504521a434a4632f3e9
lucidmachine <lucid.machine@gmail.com>
2019-03-06 08:32:11 -0500

    Use immutable array, convert UpdateFn to a type.

--- src/lib/bitclock-backend.ts
+++ src/lib/bitclock-backend.ts
@@ -1,11 +1,7 @@
 export type Bit = 0 | 1;
 export type BitDigit = [Bit, Bit, Bit, Bit];
-export type BitTime = BitDigit[];
-
-export interface UpdateFn {
-  (bitTime: BitTime): void;
-}
-
+export type BitTime = ReadonlyArray<BitDigit>;
+export type UpdateFn = (bitTime: BitTime) => void;
 
 const tensDigit = (num: number): number => Math.floor(num / 10);
 
